8a07be9b6544db75b8d920511bef600d8e116aa4
BrunoPoiano <bruno.poiano@proton.me>
2025-05-08 16:10:30 -0300

    updates config

--- config/config.go
+++ config/config.go
@@ -8,12 +8,12 @@ import (
 func LoadConfig() (*types.Config, error) {
 	return &types.Config{
 		Telegram: types.TelegramRequest{
-			Bot:    utils.ReturnEnvVariable("TELEGRAM_BOT", "7553346015:AAGygfhQZG0Vl9D_9ky-RItP2FhGRlqFpzE"),
-			ChatId: utils.ReturnEnvVariable("TELEGRAM_CHAT_ID", "1927285135"),
+			Bot:    utils.ReturnEnvVariable("TELEGRAM_BOT", ""),
+			ChatId: utils.ReturnEnvVariable("TELEGRAM_CHAT_ID", ""),
 		},
 		Sonarr: types.Sonarr{
-			Url:    utils.ReturnEnvVariable("SONARR_URL", "http://192.168.3.29:8989"),
-			ApiKey: utils.ReturnEnvVariable("SONARR_API_KEY", "0be88b49b6974ff1afcf4dc667c3d5fd"),
+			Url:    utils.ReturnEnvVariable("SONARR_URL", "http://localhost:8989"),
+			ApiKey: utils.ReturnEnvVariable("SONARR_API_KEY", ""),
 		},
 		Radarr: types.Radarr{
 			Url:    utils.ReturnEnvVariable("RADARR_URL", "http://localhost:7878"),
